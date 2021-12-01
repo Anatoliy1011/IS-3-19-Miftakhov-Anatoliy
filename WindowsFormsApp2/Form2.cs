2f8cba4d7009ceb85e05d7e2d79cd7ebee90cea9
Анатолий <secret.tolik@mail.ru>
2021-12-01 19:22:16 +0500

    Задание2

--- WindowsFormsApp2/Form2.cs
+++ WindowsFormsApp2/Form2.cs
@@ -7,19 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp2
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        class ConnBaza
         {
-            InitializeComponent();
+            public MySqlConnection ConnBaz()
+            {
+                string port = "33333";
+                string host = "caseum.ru";
+                string user = "test_user";
+                string password = "test_pass";
+                string db = "db_test";
+                string connStr = $"server={host};port={port};user={user};database={db};password={password};";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                return conn;
+            }
         }
+            public Form2()
+            {
+                InitializeComponent();
+            }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            private void button1_Click(object sender, EventArgs e)
+            {
+                ConnBaza ConnBaza = new ConnBaza();
+                try
+                {
+                    ConnBaza.ConnBaz().Open();
+                }
+                catch (Exception zxc)
+                {
+                    MessageBox.Show($"{zxc}");
+                }
+                finally
+                {
+                    MessageBox.Show("Подключение успешно");
+                    ConnBaza.ConnBaz().Close();
+                }
+            }
         }
     }
 }
