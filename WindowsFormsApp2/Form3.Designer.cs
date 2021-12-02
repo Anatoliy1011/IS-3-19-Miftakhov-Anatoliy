d1fb0f40f55d0c407f7d5dd308ad4dde335c7dde
Анатолий <secret.tolik@mail.ru>
2021-12-02 19:27:51 +0500

    Начало чётвёртой

--- WindowsFormsApp2/Form3.Designer.cs
+++ WindowsFormsApp2/Form3.Designer.cs
@@ -60,7 +60,7 @@ namespace WindowsFormsApp2
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.ClientSize = new System.Drawing.Size(600, 287);
+            this.ClientSize = new System.Drawing.Size(546, 259);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
